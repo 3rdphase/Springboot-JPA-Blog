5ebea519968629f72213026033cbc9bfe040782a
3rdphase <3rdphase@gmail.com>
2022-09-03 15:39:59 +0900

    환경설정 끝

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,15 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Getter;
+
+@RestController
+public class BlogControllerTest {
+	
+	@GetMapping(path = "/test/hello")
+	public String hello() {
+		return "Hello World!!----";
+	}
+}
